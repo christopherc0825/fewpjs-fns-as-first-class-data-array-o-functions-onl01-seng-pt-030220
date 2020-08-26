1fa20d08bfaa43f2948f6d368a68337dfe626fa6
Christopher Chaison <cchaison0@gmail.com>
2020-08-26 15:43:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function leashDog(dogName, dogBreed) {
   console.log(`Leash ${dogName} the ${dogBreed}`);
 }
 
-function walkDog(dogName, dogBreed) {
+function walkToPark(dogName, dogBreed) {
   console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
 }

91acd6a921516bb6b05fdaa1242803f5b45ed89e
Christopher Chaison <cchaison0@gmail.com>
2020-08-26 15:49:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ function unleashDog(dogName, dogBreed) {
 const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
 
 function exerciseDog(dogName, dogBreed) {
-  return routine.map(fn )
+  return routine.map(fn => fn(dogName, dogBreed))
 }
